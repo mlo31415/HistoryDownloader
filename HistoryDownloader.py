ee18bd1af87a9dd86e6cfbebad20c86c8f13e913
Mark Olson <mlo@baskerville.org>
2018-03-23 10:06:48 -0400

    Adding description

--- HistoryDownloader.py
+++ HistoryDownloader.py
@@ -1,2 +1,4 @@
 # Program to download the complete history of a Wikidot wiki and maintain a local copy.
 # The downloads are incremental and restartable.
+
+# The site history data will be kept in the directory Site History
