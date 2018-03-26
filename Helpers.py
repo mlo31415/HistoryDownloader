44d894fda737169831c1812cbe826c8116b19c70
Mark Olson <mlo@baskerville.org>
2018-03-26 17:49:21 -0400

    Add code to output information to the xml file

--- Helpers.py
+++ Helpers.py
@@ -1,27 +1,9 @@
-# Save the wiki page's metadata to an xml file
-def SaveMetadata(localName, pageData):
-    root = ET.Element("data")
-    wikiUpdatedTime = None
-    for itemName in pageData:
-        if itemName == "content" or itemName == "html":  # Skip: We've already dealt with this
-            continue
-        # Page tags get handled specially
-        if itemName == "tags":
-            tags = pageData["tags"]
-            if len(tags) > 0:
-                tagsElement = ET.SubElement(root, "tags")
-                for tag in tags:
-                    tagElement = ET.SubElement(tagsElement, "tag")
-                    tagElement.text = tag
-            continue
-        if itemName == "updated_at":  # Save the updated time
-            wikiUpdatedTime = pageData[itemName]
-        # For all other pieces of metadata, create a subelement in the xml
-        if pageData[itemName] != None and pageData[itemName] != "None":
-            element = ET.SubElement(root, itemName)
-            element.text = str(pageData[itemName])
+import datetime
 
-    # And write the xml out to file <localName>.xml.
-    tree = ET.ElementTree(root)
-    tree.write(localName + ".xml")
-    return wikiUpdatedTime
+
+def DecodeDatetime(dtstring):
+    if dtstring == None:
+        return datetime.datetime(1950, 1, 1, 1, 1, 1)    # If there's no datetime, return something early
+    if not dtstring.endswith("+00:00"):
+        raise ValueError("Could not decode datetime: '")+dtstring+"'"
+    return datetime.datetime.strptime(dtstring[:-6], '%Y-%m-%dT%H:%M:%S')
--- HistoryDownloader.py
+++ HistoryDownloader.py
@@ -1,5 +1,6 @@
 import re as Regex
 import collections
+import xml.etree.ElementTree as ET
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -20,7 +21,18 @@ from selenium.webdriver.support import expected_conditions as EC
 # xyz will contain numbered directories, each storing a single version.  I.e., xyz/2 will contain the details of version 2 of page xyz.
 # The program will guarantee that all version directories will be complete: If a directory exists, it is complete
 #   For this reason, it will be possible to prevent HistoryDownloader from attempting to download a specfic version by creating an empty version directory
-# The version directories will be as similar as possible to the directories created by FancyDownloader
+# The version directories will be similar to what is created by FancyDownloader:
+#   source.txt -- contains the source of that version of the page
+#   metadata.xml -- and xml file containing the metadata
+#           <updated_by> (name of used who did the update)
+#           <updated_at> (date and time of update)
+#           <tags> (a comma-separated list of tags)
+#           <type>  (the kind of update: new, edit, changetags, newfile, removefile, deletepage)
+#           <comment> (the update comment, if any)
+#           <title>  (the page's title)
+#           <files_deleted>  (if a file was deleted, its name)
+#           <file_list>  (a list of the files attached to thie version of this page
+#   files  -- if any files were added, the actual new files (Note that we don't keep multiple copies of files and the full set of files must be gotten by looking backwards)
 
 # Our overall strategy will be to work in two phases.
 #   In the first phase -- initial creation of the local site history -- we will run through the pages from least-recently-updated to most-recently-updated
@@ -34,56 +46,73 @@ from selenium.webdriver.support import expected_conditions as EC
 #   (The history pages are the result of javascript running and not html, so we can't use Beautiful Soup. We will try to use Selenium, which essentially contains its
 #     own internal web browser.)
 
-# Start developing the code by figuring out how to read the history of *one* test page.
+# Read and save the history of one page.
+def CreatePageHistory(browser, pageName):
+    # Open the Fancy 3 page
+    browser.get("http://fancyclopedia.org/"+pageName)
+    # Find the history button and press it
+    elem=browser.find_element_by_id('history-button')
+    elem.send_keys(Keys.RETURN)
+    # Wait until the history list has loaded
+    wait=WebDriverWait(browser, 10)
+    wait.until(EC.presence_of_element_located((By.ID, 'revision-list')))
+    # Get the history list
+    div=browser.find_element_by_xpath('//*[@id="revision-list"]/table/tbody')
+    historyElements=div.find_elements_by_xpath("tr")[1:]  # The first row is column headers, so skip them.
+    # Note that the history list is from newest to oldest
+    # The structure of a line is
+    #       The revision number followed by a "."
+    #       A series of single letters (these letters label buttons)
+    #       The name of the person who updated it
+    #       The date
+    #       An optional comment
+    # This calls for a Regex
+    rec=Regex.compile("^"  # Start at the beginning
+                      "(\d+). "  # Look for a number at least one digit long followed by a period and space
+                      "([A-Z])"  # Look for a single capital letter
+                      "( V S R | V S )"  # Look for either ' V S ' or ' V S R '
+                      "(.*)"  # Look for a name
+                      "(\d+ [A-Za-z]{3,3} 2\d{3,3})"  # Look for a date in the 2000s of the form '20 Feb 2017'
+                      "(.*)$")  # Look for an optional comment
 
-# Open the Fancy 3 page
-browser = webdriver.Firefox()
-browser.get("http://fancyclopedia.org/test")
+    for el in historyElements:
+        id=el.get_attribute("id").replace("revision-row-", "")
+        t=el.text
+        m=rec.match(t)
+        # The greedy capture of the user name captures the 1st digit of 2-digit dates.  This shows up as the user name ending in a space followed by a single digit.
+        # Fix this
+        gps=m.groups()
+        t=gps[3]
+        name=gps[3]
+        date=gps[4]
+        if t[-2:-1]==" " and t[-1:].isdigit():
+            name=t[:-2]
+            date=t[-1:]+gps[4]
 
-# Find the history button and press it
-elem = browser.find_element_by_id('history-button')
-elem.send_keys(Keys.RETURN)
 
-# Wait until the history list has loaded
-wait = WebDriverWait(browser, 10)
-wait.until(EC.presence_of_element_located((By.ID, 'revision-list')))
+        el.find_elements_by_tag_name("td")[3].find_elements_by_tag_name("a")[1].click()
+        source=div.find_element_by_xpath('//*[@id="history-subarea"]/div').text
 
-# Get the history list
-div=browser.find_element_by_xpath('//*[@id="revision-list"]/table/tbody')
-historyElements=div.find_elements_by_xpath("tr")[1:]    # The first row is column headers, so skip them.
+        # Write out the xml data
+        root=ET.Element("data")
+        el=ET.SubElement(root, "number")
+        el.text=str(gps[0])
+        el=ET.SubElement(root, "ID")
+        el.text=str(id)
+        el=ET.SubElement(root, "type")
+        el.text=str(gps[1])
+        el=ET.SubElement(root, "name")
+        el.text=str(name)
+        el=ET.SubElement(root, "date")
+        el.text=str(date)
+        el=ET.SubElement(root, "other")
+        el.text=str(gps[5])
+        # And write the xml out to file <localName>.xml.
+        tree=ET.ElementTree(root)
+        tree.write(pageName+".xml")
+        i=0
 
-# Note that the history list is from newest to oldest
-# The structure of a line is
-#       The revision number followed by a "."
-#       A series of single letters (these letters label buttons)
-#       The name of the person who updated it
-#       The date
-#       An optional comment
-# This calls for a Regex
 
-rec=Regex.compile("^"                       # Start at the beginning
-                "(\d+). "                   # Look for a number at least one digit long followed by a period and space
-                "([A-Z])"                   # Look for a single capital letter
-                "( V S R | V S )"           # Look for either ' V S ' or ' V S R '
-                "(.*)"                      # Look for a name
-                "(\d+ [A-Za-z]{3,3} 2\d{3,3})"  # Look for a date in the 2000s of the form '20 Feb 2017'
-                "(.*)$")                    # Look for an optional comment
-
-historyList=[]
-HistoryLine=collections.namedtuple("HistoryLine", "Number, ID, Type, Name, Date, Other")
-for el in historyElements:
-    id=el.get_attribute("id").replace("revision-row", "")
-    t=el.text
-    m=rec.match(t)
-    # The greedy capture of the user name captures the 1st digit of 2-digit dates.  This shows up as th used name ending in a space followed by a single digit.
-    # Fix this
-    gps=m.groups()
-    t=gps[3]
-    if t[-2:-1] == " " and t[-1:].isdigit():
-        gps=(gps[0], id, gps[1], gps[3][:-2], t[-1:]+gps[4], gps[5])
-    else:
-        gps=(gps[0], id, gps[1], gps[3], gps[4], gps[5])
-    historyList.append(HistoryLine(*gps))
-    i=0
-
+browser=webdriver.Firefox()
+CreatePageHistory(browser, "test")
 i=0
